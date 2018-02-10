f1859f4e11793bc41a45d5ac7610f0788e5cf02d
sbx_user1059 <sbx_user1059@ip-10-11-123-136.eu-west-2.compute.internal>
2018-02-10 09:38:28 +0000

    Sat Feb 10 09:38:28 UTC 2018

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -9,15 +9,15 @@ class TestCheckout(unittest.TestCase):
 
     def test_checkout_single(self):
         self.assertEqual(checkout('3B'), 75)
-
+        
+    def test_checkout_illegal(self):
+        self.assertEqual(checkout('B'), -1)
 
     def test_checkout_double(self):
         self.assertEqual(checkout('3B 1A'), 75+50)
 
     def test_checkout_with_error(self):
         self.assertEqual(checkout('3B 1A 19F'), -1)
-
-
 
     def test_checkout_with_alldata(self):
         self.assertEqual(checkout('1A 1B 1C 1D'), 50+30+20+15)
@@ -29,6 +29,9 @@ class TestCheckout(unittest.TestCase):
 
     def test_checkout_with_alldata_special_and_no(self):
         self.assertEqual(checkout('4A 3B 19D'), 130+50+45+30+(19*15))
+
+    def test_checkout_with_alldata_10(self):
+        self.assertEqual(checkout('10A 10B 10C 10D'), (3*130+50)+(5*45)+10*20+10*15)
 
 if __name__ == '__main__':
     unittest.main()
