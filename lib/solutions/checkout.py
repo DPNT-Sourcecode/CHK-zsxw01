1d3cd2f86c03556cfdb3382e458dc3c1122a79a0
sbx_user1059 <sbx_user1059@ip-10-11-123-136.eu-west-2.compute.internal>
2018-02-10 09:05:28 +0000

    Sat Feb 10 09:05:28 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,39 @@
+"""
 
+Our price table and offers:
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+
+
+Notes:
+ - For any illegal input return -1
+
+In order to complete the round you need to implement the following method:
+     checkout(String) -> Integer
+
+Where:
+ - param[0] = a String containing the SKUs of all the products in the basket
+ - @return = an Integer representing the total checkout value of the items
+
+"""
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+
+    # split skus
+
+
+    # loop sku_list
+
+        # check special offers
+    
+        # check NO special offers
+
     raise NotImplementedError()
